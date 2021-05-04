057281eacd0fd98050ad702e8a088f1b9eb656e4
kou <suhrr.code@gmail.com>
2021-05-04 22:28:25 +0900

    add env

--- main.go
+++ main.go
@@ -2,26 +2,29 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"github.com/joho/godotenv"
 	"golang.org/x/text/unicode/norm"
 )
 
-const (
-	SRC_DIR_PATH       = "./iTunes/"
-	DEST_DIR_PATH      = "./walkman/"
-	ITUNES_MUSIC_PATH  = "/Volumes/HDD/iTunes/iTunes Media/music/"
-	WALKMAN_MUSIC_PATH = "/MUSIC/Music/"
-)
+// 環境変数
+var envs = map[string]string{"SRC_DIR_PATH": "", "DEST_DIR_PATH": "", "ITUNES_MUSIC_PATH": "", "WALKMAN_MUSIC_PATH": ""}
 
 func main() {
 	fmt.Println("createing...")
 
-	files, _ := filepath.Glob(SRC_DIR_PATH + "*.m3u")
+	err := loadEnv()
+	if err != nil {
+		panic(err)
+	}
+
+	files, _ := filepath.Glob(envs["SRC_DIR_PATH"] + "*.m3u")
 	if len(files) == 0 {
 		fmt.Println("not found")
 		os.Exit(0)
@@ -38,6 +41,24 @@ func main() {
 	fmt.Println("finish!")
 }
 
+// .envファイルを読み込みグローバル変数にセットしていく
+func loadEnv() error {
+	err := godotenv.Load(".env")
+	if err != nil {
+		return fmt.Errorf("error: %w", err)
+	}
+
+	for index := range envs {
+		env := os.Getenv(index)
+		if env == "" {
+			return errors.New(index + " is empty")
+		}
+		envs[index] = env
+	}
+
+	return nil
+}
+
 // ウォークマン用のプレイリストを生成する
 func createPlayList(srcPath string) error {
 	srcFile, err := os.Open(srcPath)
@@ -46,7 +67,7 @@ func createPlayList(srcPath string) error {
 	}
 	defer srcFile.Close()
 
-	destFile, err := os.Create(DEST_DIR_PATH + filepath.Base(srcPath))
+	destFile, err := os.Create(envs["DEST_DIR_PATH"] + filepath.Base(srcPath))
 	if err != nil {
 		return fmt.Errorf("error: %w", err)
 	}
@@ -62,7 +83,7 @@ func createPlayList(srcPath string) error {
 		}
 
 		// パスを置換
-		replaced := strings.Replace(line, ITUNES_MUSIC_PATH, WALKMAN_MUSIC_PATH, -1)
+		replaced := strings.Replace(line, envs["ITUNES_MUSIC_PATH"], envs["WALKMAN_MUSIC_PATH"], -1)
 		// macはNFD形式なのでNFC形式に変換
 		replaced = norm.NFC.String(replaced)
 
